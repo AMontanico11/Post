bf9c2e970dcf92a555712a3d0a7d810ed24e44a0
Montanico <afpirir@scl.edu.gt>
2022-06-06 22:11:48 -1200

    Primer proyecto en equipo con las ramas

---
+++ js/Main.js
@@ -0,0 +1,21 @@
+let image = document.querySelector('.image');
+let select = document.getElementById('effect');
+
+select.addEventListener('change', function (e) {
+  image.className = `image ${this.value}`;
+});
+
+let image2 = document.querySelector('.image2');
+let select2 = document.getElementById('effect');
+
+select.addEventListener('change', function (e) {
+  image.className = `image ${this.value}`;
+});
+
+
+let image3 = document.querySelector('.image3');
+let select3 = document.getElementById('effect');
+
+select.addEventListener('change', function (e) {
+  image.className = `image ${this.value}`;
+});
